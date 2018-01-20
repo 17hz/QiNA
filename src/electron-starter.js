c038c2cee8a47a446ca52813d1ff66db8aee47ee
Retroxs <retroxsx@gmail.com>
2018-01-20 14:08:57 +0800

    add: some npm script

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -32,7 +32,7 @@ function createWindow () {
 
   // and load the index.html of the app.
 const startUrl = process.env.ELECTRON_START_URL || url.format({
-  pathname: path.join(__dirname, '/../build/index.html'),
+  pathname: path.join(__dirname, '/../dist/index.html'),
   protocol: 'file:',
   slashes: true
 });
